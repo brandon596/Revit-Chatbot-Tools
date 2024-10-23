a0cc5c197e32fba60661d5c19e96e4b116e99c09
Brandon <you@example.com>
2024-10-23 15:56:19 +0800

    updated

--- getVideo_links.py
+++ getVideo_links.py
@@ -7,25 +7,26 @@ def load_json_file():
     return data
 
 def retrieve_urls(citations: list):
-    links_out = {"Video_Links": [], "Subtitle": []}
     video_links = load_json_file()
     media_urls = set()
     subtitles = set()
     for row in video_links:
         source_to_show = row["Page URL"]
-        # related_links = None #Temp
         related_links = row["Related URLs"]
         video_URL = row["Video URL"]
-        all_urls = [source_to_show, related_links, video_URL]
+        all_urls = source_to_show + related_links + video_URL
         for citation in citations:
             if citation["Url"] in all_urls:
                 media_urls.add(video_URL)
                 subtitles.add(source_to_show)
-    
-    if len(media_urls) > 0:
-        links_out = {"Video_Links": [{"url": link} for link in media_urls], "Subtitle": [{"url": link} for link in subtitles]}
-        links_out["qty"] = len(media_urls)
-        return links_out
-    else:
-        #return "nil"
-        return links_out #temp
+    links_out = {"Video_Links": [{"url": link} for link in media_urls], "Subtitle": [{"url": link} for link in subtitles]}
+    links_out["qty"] = len(media_urls)
+
+    return links_out
+    # if len(media_urls) > 0:
+    #     links_out = {"Video_Links": [{"url": link} for link in media_urls], "Subtitle": [{"url": link} for link in subtitles]}
+    #     links_out["qty"] = len(media_urls)
+    #     return links_out
+    # else:
+    #     #return "nil"
+    #     return links_out #tempget
